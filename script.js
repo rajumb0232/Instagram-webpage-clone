c830e8a678f0d9403b7f79f0853ba3e3ca7a1634
rajumb0232 <rajugowda0232@gmail.com>
2023-12-23 17:50:56 +0530

    First commit - top and bottom navigation bar done | stories section done

---
+++ script.js
@@ -0,0 +1,157 @@
+let posts=[
+    {
+        "id":1,
+        "image":"https://images.unsplash.com/photo-1495211895963-08d8812dcbf0?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "caption":"Stepping into the future, but my best life stays in every footprint. ⚡🚀",
+        "hashtags":"#life #positivity #goals",
+        "likes" : 1547,
+        "comments":300,
+        "user":{
+            "name":"Jakob_Owens",
+            "image":"https://images.unsplash.com/photo-1579540252588-da0ee883560d?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        }
+    },
+    {
+        "id":2,
+        "image":"https://plus.unsplash.com/premium_photo-1667340456421-e39b77a25217?q=80&w=3507&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "caption":"Hope you have a magical day! ❤️ ",
+        "hashtags":"#happy #loved",
+        "likes" : 38624,
+        "comments":3467,
+        "user":{
+            "name":"Igor_Son",
+            "image":"https://images.unsplash.com/photo-1501004318641-b39e6451bec6?q=80&w=3773&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        }
+    },
+    {
+        "id":3,
+        "image":"https://images.unsplash.com/photo-1495107334309-fcf20504a5ab?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "caption":"When you have to take off from a high point, always get used to height and then plan to fly! 🍃",
+        "hashtags":"#nature #green #Photography #NaturePhotography #lens #lifeThroughLens",
+        "likes" : 42949,
+        "comments":507,
+        "user":{
+            "name":"Simon_Reza",
+            "image":"https://images.unsplash.com/profile-1695394970564-c78b5aaff4b5image?bg=fff&crop=faces&dpr=2&h=150&w=150&auto=format&fit=crop&q=60&ixlib=rb-4.0.3"
+        }
+    },
+    {
+        "id":4,
+        "image":"https://images.unsplash.com/photo-1490191943407-e44bafe496ac?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "caption":"so cute..! 🥹❤️...",
+        "hashtags":"#cute #cows #farmLife",
+        "likes" : 2420098,
+        "comments":32,
+        "user":{
+            "name":"tevin_trinh",
+            "image":"https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        }
+    },
+    {
+        "id":5,
+        "image":"https://images.unsplash.com/photo-1613755317778-0003e3bfc2c5?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "caption":"Magestic 👑🐦🤍...",
+        "hashtags":"#BirdsOfIndia #Birds #birdsPhotography #Photography #motherNature",
+        "likes" : 953783,
+        "comments":987,
+        "user":{
+            "name":"Nayan_Sharma",
+            "image":"https://images.unsplash.com/photo-1526313199968-70e399ffe791?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        }
+    },
+    {
+        "id":6,
+        "image":"https://images.unsplash.com/photo-1489440543286-a69330151c0b?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "caption":"There are places that never get bored and there are companies as well... ",
+        "hashtags":"#DogForLife #dog #MyLife #nature #travel",
+        "likes" : 25494,
+        "comments":476,
+        "user":{
+            "name":"Aron_Wu",
+            "image":"https://images.unsplash.com/photo-1470675088316-e747a6f3c879?q=80&w=3874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        }
+    },
+    {
+        "id":7,
+        "image":"https://images.unsplash.com/photo-1508179640279-deaa2ea35e82?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "caption":"Happiness is a choice 💫",
+        "hashtags":"#Happiness #happinessIsAChoice #redBlue #enjoy",
+        "likes" : 2674,
+        "comments":105,
+        "user":{
+            "name":"devidalan",
+            "image":"https://images.unsplash.com/photo-1488027236809-dc613266478a?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        }
+    },
+    {
+        "id":8,
+        "image":"https://images.unsplash.com/photo-1622052354119-4be9fa40b26d?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "caption":"There has been a fly in there for three days. So far, no one has caught it.😅",
+        "hashtags":"CatsOfInsta #Cats #coke #orange #cuteHunter😍",
+        "likes" : 253,
+        "comments":23,
+        "user":{
+            "name":"cats_of_instagram",
+            "image":"https://plus.unsplash.com/premium_photo-1677545182067-26ac518ef64f?q=80&w=3956&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        }
+    },
+    {
+        "id":9,
+        "image":"https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "caption":"Unveiling the beauty that lies within, happy tuesday everyone...",
+        "hashtags":"#model #fashionMadel #clothes #photography #portfolio",
+        "likes" : 258754,
+        "comments":4350,
+        "user":{
+            "name":"im_alice",
+            "image":"https://images.unsplash.com/photo-1509967419530-da38b4704bc6?q=80&w=3895&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        }
+    },
+    {
+        "id":10,
+        "image":"https://images.unsplash.com/photo-1512453979798-5ea266f8880c?q=80&w=2400&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "caption":"Sunset",
+        "hashtags":"#city #evening #photography #sunsetPhotography #city #sky #skyScrappers",
+        "likes" : 7845,
+        "comments":139,
+        "user":{
+            "name":"photo.dump",
+            "image":"https://images.unsplash.com/photo-1519501025264-65ba15a82390?q=80&w=3764&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        }
+    }
+]   
+
+
+
+// let storyImage = document.querySelector(".story-profile-img");
+// let storyName = document.querySelector(".story-profile-name");
+let storyBar = document.querySelector(".stories-bar");
+
+let stories = [];
+
+posts.forEach(post => {
+    let user = post.user;
+
+    let story = document.createElement("div");
+    story.classList.add("story");
+  
+    let image = document.createElement("img");
+    image.src = user.image;
+    image.classList.add("story-profile-img");
+    story.appendChild(image);
+  
+    let name = document.createElement("p");
+    name.textContent = user.name;
+    name.classList.add("story-profile-name");
+    story.appendChild(name);
+  
+    storyBar.appendChild(story);
+
+  });
+
+// let story=document.querySelector(".story")
+// posts.map((e)=>{
+//     console.log(e)
+//     story.innerHTML=`<img ${e.image}>`
+// })
+
